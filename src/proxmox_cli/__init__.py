bc07e2ed67dc749c17b6dd4825896b6a84d0c926
Ralph Brynard <ralph.brynard@gmail.com>
2025-11-13 18:08:49 -0600

    chore: bump version to 0.4.2

--- src/proxmox_cli/__init__.py
+++ src/proxmox_cli/__init__.py
@@ -1,5 +1,5 @@
 """Proxmox CLI - Command-line interface for Proxmox Virtual Environment."""
 
-__version__ = "0.5.0"
+__version__ = "0.4.2"
 __author__ = "Ralph Brynard"
 __email__ = "ralph.brynard@gmail.com"
